f12e101e01df07e1ed1fbe8c64ace08727b2ef1a
lanser <a.doniyor@aliftech.uz>
2023-02-17 03:04:27 +0500

    add return type ElasticEloquentInterface

--- src/ElasticEloquent/ElasticEloquent.php
+++ src/ElasticEloquent/ElasticEloquent.php
@@ -2,24 +2,32 @@
 
 namespace Lanser\Elastic\ElasticEloquent;
 
-use App\Services\MyElastic\ElasticEloquent\ElasticEloquentInterface\ElasticEloquentInterface;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
+use Lanser\Elastic\ElasticEloquent\ElasticEloquentInterface\ElasticEloquentInterface;
+use Lanser\Elastic\ElasticSearchBuilder\Interfce\ElasticBuilderInterface;
 use stdClass;
 
 class ElasticEloquent implements ElasticEloquentInterface
 {
     public stdClass $elasticItem;
 
-    public function __construct(stdClass $elasticItem)
+    protected function reset(): void
     {
-        $this->elasticItem = $elasticItem;
+        $this->elasticItem = null;
     }
+    public function SetStdClass(stdClass $elasticItem): ElasticEloquentInterface
+    {
+        $this->reset();
+        $this->elasticItem = $elasticItem;
+        return $this;
+    }
+
 
     public function paginate(int $perPage = 1, int $page = 1): LengthAwarePaginator
     {
-        $items = $this->getItems();
+        $items = $this->elasticItem;
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
         $items = $items instanceof Collection ? $items : Collection::make($items);
         return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, [
@@ -28,28 +36,28 @@ class ElasticEloquent implements ElasticEloquentInterface
         ]);
     }
 
-    public function find(int $id)
+    public function find(int $id): ElasticEloquentInterface
     {
         $items = $this->getCollection();
         return $items->where("_id", "=", $id)->first();
     }
 
-    public function getTook()
+    public function getTook(): ElasticEloquentInterface
     {
         return $this->elasticItem->took;
     }
 
-    public function getShards()
+    public function getShards(): ElasticEloquentInterface
     {
         return $this->elasticItem->_shards;
     }
 
-    public function getTotal()
+    public function getTotal(): ElasticEloquentInterface
     {
         return $this->elasticItem->hits->total->value;
     }
 
-    public function getItems()
+    public function getItems(): ElasticEloquentInterface
     {
         return $this->elasticItem->hits->hits;
     }
--- src/ElasticEloquent/ElasticEloquentInterface/ElasticEloquentInterface.php
+++ src/ElasticEloquent/ElasticEloquentInterface/ElasticEloquentInterface.php
@@ -3,9 +3,11 @@
 namespace Lanser\Elastic\ElasticEloquent\ElasticEloquentInterface;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use stdClass;
 
 interface ElasticEloquentInterface
 {
+    public function SetStdClass(stdClass $elasticItem): ElasticEloquentInterface;
     public function paginate( int $perPage = 1, int $page = 1): LengthAwarePaginator;
 
     public function find(int $id);
