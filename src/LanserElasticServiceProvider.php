1ce60664eb0d843f19b95d691c809c9dab8ded4f
lanser <a.doniyor@aliftech.uz>
2023-02-17 02:55:51 +0500

    add some logic to serviceProvider

--- src/LanserElasticServiceProvider.php
+++ src/LanserElasticServiceProvider.php
@@ -2,9 +2,20 @@
 
 namespace Lanser\Elastic;
 use Illuminate\Support\ServiceProvider;
+use Lanser\Elastic\ElasticConnect\ElasticConnect;
+use Lanser\Elastic\ElasticSearchBuilder\ElasticBuilder;
+use Lanser\Elastic\ElasticSearchBuilder\Interfce\ElasticBuilderInterface;
 
 class LanserElasticServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->app->bind(ElasticConnect::class, function () {
+            return (new ElasticConnect())->getElasticClient();
+        });
+        $this->app->bind(ElasticBuilderInterface::class, ElasticBuilder::class);
+    }
+
     public function boot()
     {
         if ($this->app->runningInConsole()) {
